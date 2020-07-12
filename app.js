2f62b952374924b998afea336ddf01858645843b
jatin2001 <jatinkumarjk2001@gmail.com>
2020-07-13 00:23:44 +0530

    m

--- app.js
+++ app.js
@@ -120,11 +120,7 @@ app.get("/:paramName",(req,res)=>{
 })
 
 
-let port = process.env.port;
-if(port==null|| port=='')
-{
-    port:3000
-}
-app.listen(port,()=>{
+
+app.listen(process.env.port||3000,()=>{
     console.log('Server is running on port 3000');
 })
